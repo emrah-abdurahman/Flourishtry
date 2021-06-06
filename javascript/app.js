c9929a274ad65ef1e92e458cea5fa05cf050776e
Emrah Abdurahman <Emrah.Abdurahman@hotmail.com>
2021-06-06 18:41:28 +0100

    fix link hover effects

--- javascript/app.js
+++ javascript/app.js
@@ -1,7 +1,31 @@
+// Get the home link element which is the Flourishtry logo in the top left of the navbar
 const homeLink = document.getElementById("home-link");
-homeLink.addEventListener("mouseover", () => {
-  homeLink.classList.add("mouse-over");
+// Get all navigation links
+const navLinks = document.getElementsByClassName("nav-link");
+console.log(navLinks);
+
+// Add a mouseover event listener to the Flourishtry home logo
+// This will add and remove the appropriate CSS classes
+homeLink.addEventListener("mouseover", function () {
+  homeLink.classList.add("home-link-mouse-over");
+  homeLink.classList.remove("home-link-mouse-out");
 });
-homeLink.addEventListener("mouseout", () => {
-  homeLink.classList.add("mouse-out");
+
+// Add a mouseout event listener to the Flourishtry home logo
+// This will add and remove the appropriate CSS classes
+homeLink.addEventListener("mouseout", function () {
+  homeLink.classList.add("home-link-mouse-out");
+  homeLink.classList.remove("home-link-mouse-over");
 });
+
+// Use a for of loop to iterate through the HTMLCollection and add the appropriate event listeners to each navigation link
+for (let navLink of navLinks) {
+  navLink.addEventListener("mouseover", function () {
+    navLink.classList.add("nav-link-mouse-over");
+    navLink.classList.remove("nav-link-mouse-out");
+  });
+  navLink.addEventListener("mouseout", function () {
+    navLink.classList.add("nav-link-mouse-out");
+    navLink.classList.remove("nav-link-mouse-over");
+  });
+}
