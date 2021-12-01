c9e3e4b32e7faf2a1691b3e16d483d5fa4bc5dc8
Emrah Abdurahman <Emrah.Abdurahman@hotmail.com>
2021-12-01 14:47:25 +0000

    create navigation.js file and move some of the navigation logic from app.js

---
+++ javascript/navigation.js
@@ -0,0 +1,19 @@
+/**
+ * navigation.js contains all of the logic for the site's navigation
+ * which is implemented across the site
+ **/
+
+// Get the home link element which is the Flourishtry logo in the top left of the navbar
+const homeLink = document.querySelector("#home-link");
+
+// Add a mouseover and mouseout event listener to the Flourishtry home logo
+// This will add and remove the appropriate CSS classes
+homeLink.addEventListener("mouseover", function (event) {
+  event.stopPropagation();
+  console.log(this);
+  const randomColor = Math.floor(Math.random() * 6) + 1;
+  homeLink.style.borderBottom = `3px solid ${colors[randomColor]}`;
+});
+homeLink.addEventListener("mouseout", function () {
+  homeLink.style.borderBottom = "";
+});
